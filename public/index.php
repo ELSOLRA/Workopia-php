3d345429d8302106b00fa2b9f60ba1e71dd4fb33
ELSOLRA <bardakasmiljo@gmail.com>
2024-12-08 22:41:59 +0100

    create Database class  and initiating database in home.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,18 @@
 
 <?php
 require '../helpers.php';
-
+require basePath('Database.php');
 require basePath('Router.php');
 
+// initiating the router
 $router = new Router();
 
+// get routes
 $routes = require basePath('routes.php');
 
+// get current URI and HTTP methods
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
