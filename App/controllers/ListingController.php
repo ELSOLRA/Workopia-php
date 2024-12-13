1f7cddde1a1895cf80888e3e942fceb4acb330a5
ELSOLRA <bardakasmiljo@gmail.com>
2024-12-13 20:41:55 +0100

    fuction route redone to take/read {id}

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -25,7 +25,7 @@ class ListingController
         $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
 
-        loadView('home', [
+        loadView('listings/index', [
             'listings' => $listings
         ]);
     }
@@ -49,14 +49,20 @@ class ListingController
      */
 
 
-    public function show()
+    public function show($params)
     {
-        $id = $_GET['id'] ?? '';
+        $id = $params['id'] ?? '';
         $params = [
             'id' => $id
         ];
 
         $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        // Check if listing exists
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
 
 
         loadView('listings/show', ['listing' => $listing]);
--- public/index.php
+++ public/index.php
@@ -26,7 +26,5 @@ $routes = require basePath('routes.php');
 // get current URI and HTTP methods
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$method = $_SERVER['REQUEST_METHOD'];
-
 // Route the request
-$router->route($uri, $method);
+$router->route($uri);
