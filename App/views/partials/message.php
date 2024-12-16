2783b5cb7300725e61a5d624bc5d8b2cec7e2794
ELSOLRA <bardakasmiljo@gmail.com>
2024-12-16 11:17:32 +0100

    refactoring flash massages

--- App/views/partials/message.php
+++ App/views/partials/message.php
@@ -1,13 +1,18 @@
-        <?php if (isset($_SESSION['success_message'])) : ?>
-            <div class="message bg-green-100 p-3 my-3">
-                <?= $_SESSION['success_message'] ?>
-            </div>
-            <?php unset($_SESSION['success_message']) ?>
-        <?php endif; ?>
+        <?php
 
-        <?php if (isset($_SESSION['error_message'])) : ?>
-            <div class="message bg-red-200 p-3 my-3">
-                <?= $_SESSION['error_message'] ?>
+        use Framework\Session;
+
+        $successMessage = Session::getFlashMessage('success_message');
+        $errorMessage = Session::getFlashMessage('error_message');
+        ?>
+
+        <?php if ($successMessage !== null) : ?>
+            <div class="message bg-green-100 p-3 my-3">
+                <?= $successMessage ?>
             </div>
-            <?php unset($_SESSION['error_message']) ?>
+        <?php endif; ?>
+        <?php if ($errorMessage !== null) : ?>
+            <div class="message bg-red-100 p-3 my-3">
+                <?= $errorMessage ?>
+            </div>
         <?php endif; ?>
