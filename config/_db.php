6fb0fc71d43ea4cba57260a5c8e3540a3f13fa50
Paul <133661756+ELSOLRA@users.noreply.github.com>
2024-12-29 14:14:57 +0100

    database configuration example

---
+++ config/_db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'add_database_name',
+    'username' => 'add_DB_username',
+    'password' => 'add_DB_password',
+];
